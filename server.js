8c78a2b58289c27330414f9a4b8af411582050c3
nalcakar <nalcakar@gmail.com>
2025-03-28 09:51:54 +0300

    Temiz başlangıç

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://your-app-name.onrender.com"],
+  origin: ["http://localhost:3000", "https://questionbase-o6jk.onrender.com"],
   credentials: true
 }));
 app.use(express.json());
